5ed261d89746f95c0920860b430037da2ab2abe7
Priyesh <Macbook@Priyeshs-MacBook-Pro.local>
2017-02-26 17:12:01 +0530

    /////

--- connector.php
+++ connector.php
@@ -10,10 +10,6 @@ $host = $url["host"];
 
 // Create connection
 $conn = new mysqli($host,$user,$password,$db);
-
-// // Check connection
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// } 
+mysqli_select_db($conn,$db);
 
 ?>
